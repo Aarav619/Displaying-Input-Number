92d7f2feeb3f4df16fa68ba5e6d1031500834ac0
Aarav619 <66662179+Aarav619@users.noreply.github.com>
2021-05-06 15:29:23 +0530

    Update main.c

--- Printing_Entered_Numbers_Through_Recursion/main.c
+++ Printing_Entered_Numbers_Through_Recursion/main.c
@@ -1,1 +1,22 @@
-
+#include<stdio.h>
+void main(void)
+{
+  void show(int);
+	int num;
+	printf("Enter the Number :");
+	scanf("%d",&num);
+	printf("You Entered: ");
+	show(num);	
+	printf("\n");
+}
+void show(int x)
+{
+  if(x/10 == 0) //if Condicton executes when number is single digit Number 
+  {
+    printf("%d",x%10);  //if x is single Digit Number then n%10 returns the same Number 
+    return; //returns to previous function calls
+  }
+  //Using Double Recursion
+  show(x/10);
+  show(x%10);
+}
